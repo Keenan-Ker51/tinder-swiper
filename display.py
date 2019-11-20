0bf1114509d7a929df54ea3d407343eab977f647
Keenan-Ker51 <keenan.ker51@gmail.com>
2019-11-20 14:36:21 -0500

    Final changes should be all working now

--- display.py
+++ display.py
@@ -3,6 +3,7 @@ from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import *
 from pynput.keyboard import Key, Controller
+from pytesseract import image_to_string
 import time
 import random
 from image_proc import *
@@ -51,17 +52,31 @@ class mymainwindow(QtGui.QMainWindow):
             with mss.mss() as sct:
                 # The screen part to capture
                 monitor = {"top": 450, "left": 750, "width": 160, "height": 135}
-                output = "screenshots/user_{}.png".format(state)
+                if state=="start":
+                    svmon = {"top": 450, "left": 750, "width": 300, "height": 400}
+                    output = "screenshots/user_{}.png".format(state)
+                    sv = "names/{}".format{}
 
-                # Grab the data
-                sct_img = sct.grab(monitor)
+                    # Grab the data
+                    sct_img = sct.grab(monitor)
 
-                # Save to the picture file
-                mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-                #print(output)
+                    # Save to the picture file
+                    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+                    #print(output)
+                else:
+                    output = "screenshots/user_{}.png".format(state)
+
+                    # Grab the data
+                    sct_img = sct.grab(monitor)
+
+                    # Save to the picture file
+                    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+                    #print(output)
 
         def on_click(self):
+            QApplication.processEvents()
             textbox.setText("Button clicked.")
+            QApplication.processEvents()
             #get_pics("start")
             #pic1.setPixmap(QtGui.QPixmap("screenshots/user_start.png"))
             #pic1.setPixmap(QtGui.QPixmap("sct-{top}x{left}_{width}x{height}.png".format(**monitor)))
@@ -74,18 +89,23 @@ class mymainwindow(QtGui.QMainWindow):
         button.clicked.connect(on_click)
         def show_curpic(self):
             get_pics("now")
+            QApplication.processEvents()
             pic2.setPixmap(QtGui.QPixmap("screenshots/user_now.png"))
             pic2.show()
+            QApplication.processEvents()
         def swiping():
             time.sleep(1)
             while True:
                 count = 0
                 time.sleep((random.randint(1,10)/10)+1)
                 get_pics("start")
+                QApplication.processEvents()
                 pic1.setPixmap(QtGui.QPixmap("screenshots/user_start.png"))
                 pic1.show()
+                QApplication.processEvents()
                 print(return_avgs())
                 pic_dif.setText(str(return_avgs()))
+                QApplication.processEvents()
                 while return_avgs() >2 or return_avgs() <-2 and count <8:
                     print(return_avgs())
                     swiper.press(Key.space)
@@ -95,6 +115,7 @@ class mymainwindow(QtGui.QMainWindow):
                     count = count+1
                     print(return_avgs())
                     pic_dif.setText(str(return_avgs()))
+                    QApplication.processEvents()
                 swiper.press(Key.right)
                 time.sleep(0.1)
                 swiper.release(Key.right)
